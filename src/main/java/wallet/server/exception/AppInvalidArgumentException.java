750aabcf1e8e09cb34a6d54aac450f72298d9d7c
Megha-Namdeo <namdeo.megha2905@gmail.com>
2019-11-07 22:37:31 +0000

    added wallet service impl

---
+++ src/main/java/wallet/server/exception/AppInvalidArgumentException.java
@@ -0,0 +1,12 @@
+package wallet.server.exception;
+
+public class AppInvalidArgumentException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public AppInvalidArgumentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/java/wallet/server/service/WalletService.java
+++ src/main/java/wallet/server/service/WalletService.java
@@ -1,5 +1,7 @@
 package wallet.server.service;
 
+import java.math.BigDecimal;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,9 @@ import com.server.grpc.WalletServiceGrpc.WalletServiceImplBase;
 
 import io.grpc.stub.StreamObserver;
 import wallet.server.dao.WalletRepository;
+import wallet.server.entity.Currency;
 import wallet.server.entity.Wallet;
+import wallet.server.exception.AppInvalidArgumentException;
 
 @Service
 public class WalletService extends WalletServiceImplBase {
@@ -23,19 +27,134 @@ public class WalletService extends WalletServiceImplBase {
 	@Transactional
 	public void balance(WalletRequest request, StreamObserver<WalletResponse> responseObserver) {
 
-		Wallet userWallet = walletRepository.findById(request.getUserID()).orElse(null);
+		try {
 
-		if (userWallet != null) {
+			Wallet userWallet = validateUser(request);
 
 			String balance = userWallet.getBalance().toPlainString();
 
 			responseObserver
 					.onNext(WalletResponse.newBuilder().setResponsemessage(balance).setResponsecode(100).build());
 
-		} else {
+		} catch (AppInvalidArgumentException e) {
 
-			responseObserver.onError(new Throwable("User Wallet is Null"));
+			responseObserver.onError(new Throwable(e.getMessage()));
+
 		}
+
+	}
+
+	@Override
+	@Transactional
+	public void withdraw(WalletRequest request, StreamObserver<WalletResponse> responseObserver) {
+
+		try {
+			Wallet userWallet = validateUser(request);
+
+			validateCurrency(request);
+
+			BigDecimal withdrawAmount = validateAmount(request, userWallet.getBalance());
+
+			BigDecimal newAmount = userWallet.getBalance().subtract(withdrawAmount);
+
+			userWallet.setBalance(newAmount);
+
+			walletRepository.save(userWallet);
+
+			responseObserver.onNext(WalletResponse.newBuilder().setResponsemessage(newAmount.toPlainString()).build());
+
+		} catch (AppInvalidArgumentException e) {
+
+			responseObserver.onError(new Throwable(e.getMessage()));
+
+		}
+
+	}
+
+	@Override
+	@Transactional
+	public void deposit(WalletRequest request, StreamObserver<WalletResponse> responseObserver) {
+
+		try {
+
+			Wallet userWallet = validateUser(request);
+
+			validateCurrency(request);
+			
+			BigDecimal depositAmount = new BigDecimal(request.getAmount());
+
+			BigDecimal newAmount = userWallet.getBalance().add(depositAmount);
+
+			userWallet.setBalance(newAmount);
+
+			walletRepository.save(userWallet);
+
+			responseObserver.onNext(WalletResponse.newBuilder().setResponsemessage(newAmount.toPlainString()).build());
+
+		} catch (AppInvalidArgumentException e) {
+
+			responseObserver.onError(new Throwable(e.getMessage()));
+
+		}
+
+	}
+
+	private BigDecimal validateAmount(WalletRequest request, BigDecimal balance) throws AppInvalidArgumentException {
+
+		if (request.getAmount().isEmpty()) {
+
+			throw new AppInvalidArgumentException("Amount should be provided for the operation");
+
+		}
+
+		BigDecimal amount = new BigDecimal(request.getAmount());
+
+		if (amount.compareTo(BigDecimal.ZERO) < 1) {
+
+			throw new AppInvalidArgumentException("Amount should be greater than 0");
+
+		}
+
+		if (balance.compareTo(amount) < 0) {
+
+			throw new AppInvalidArgumentException("Insufficient funds for Withdrawal Operation ");
+
+		}
+
+		return amount;
+
+	}
+
+	private void validateCurrency(WalletRequest request) throws AppInvalidArgumentException {
+
+		if (request.getCurrency().isEmpty()) {
+
+			throw new AppInvalidArgumentException("Currency should be provided for the operation ");
+
+		}
+
+		try {
+
+			Currency.valueOf(request.getCurrency().toUpperCase());
+
+		} catch (IllegalArgumentException e) {
+
+			throw new AppInvalidArgumentException("Currency should be in 'USD/EUR/GBP' ");
+
+		}
+	}
+
+	private Wallet validateUser(WalletRequest request) throws AppInvalidArgumentException {
+
+		Wallet wallet = walletRepository.findById(request.getUserID()).orElse(null);
+
+		if (wallet == null) {
+
+			new AppInvalidArgumentException("User Doesnot exist");
+
+		}
+
+		return wallet;
 
 	}
 
